6cd92f559f5e9421d6daf16746047f476c5f188d
NeoxAzrot <sami.lafrance@akwai.co>
2023-11-04 14:09:02 +0100

    feat(Producer): add single

--- front/src/app/(default)/producers/[slug]/page.tsx
+++ front/src/app/(default)/producers/[slug]/page.tsx
@@ -4,10 +4,13 @@ import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 import Cta from '@/components/cta-dark';
+import Mdx from '@/components/mdx/mdx';
 import PostDate from '@/components/post-date';
 import Separator from '@/components/separator';
 
 import { getAllProducers, getOneProducerBySlug } from '@/services/producer';
+
+import { AnimalSurvey, StoreSurvey, TransformSurvey, VegetalSurvey } from '@/types/surveys';
 
 export const generateStaticParams = async () => {
   const producers = await getAllProducers({});
@@ -56,6 +59,526 @@ export const generateMetadata = async ({
   };
 };
 
+const AnimalSurveyRender = (survey: AnimalSurvey) => {
+  return (
+    <>
+      {survey.mainProducerValues && (
+        <>
+          <h3>
+            En trois mots : quelles sont les valeurs/idées principales portées par le producteur ?
+          </h3>
+          <p>{survey.mainProducerValues}</p>
+
+          <Separator />
+        </>
+      )}
+
+      {(survey.rearingMode ||
+        survey.feedSource ||
+        survey.antibioticTreatment ||
+        survey.welfareMeasures ||
+        survey.agroecologicalPractices) && (
+        <div>
+          <h2>Mode d&apos;élevage</h2>
+
+          {survey.rearingMode && (
+            <>
+              <h3>
+                Quel est le mode d&apos;élevage des animaux ? (Hors sol, accès plein air, intensif,
+                extensif, ...)
+              </h3>
+              <Mdx code={survey.rearingMode} />
+            </>
+          )}
+
+          {survey.feedSource && (
+            <>
+              <h3>D&apos;où viennent les aliments donnés aux animaux ?</h3>
+              <Mdx code={survey.feedSource} />
+            </>
+          )}
+
+          {survey.antibioticTreatment && (
+            <>
+              <h3>Les animaux sont-ils traités avec des antibiotiques ?</h3>
+              <Mdx code={survey.antibioticTreatment} />
+            </>
+          )}
+
+          {survey.welfareMeasures && (
+            <>
+              <h3>
+                Des choses sont-elles mises en place pour le bien-être animal ? Si oui, lesquelles ?
+              </h3>
+              <Mdx code={survey.welfareMeasures} />
+            </>
+          )}
+
+          {survey.agroecologicalPractices && (
+            <>
+              <h3>
+                Des pratiques agroécologiques sont-elles mises en place ? Si oui, lesquelles ?
+              </h3>
+              <Mdx code={survey.agroecologicalPractices} />
+            </>
+          )}
+
+          <Separator />
+        </div>
+      )}
+
+      {(survey.effluentManagement ||
+        survey.wasteManagementProduction ||
+        survey.wasteManagementSales ||
+        survey.resourceConsumptionManagement ||
+        survey.carbonImpactMeasurement) && (
+        <div>
+          <h2>Environnement et ressources</h2>
+
+          {survey.effluentManagement && (
+            <>
+              <h3>Comment sont gérés les effluents ?</h3>
+              <Mdx code={survey.effluentManagement} />
+            </>
+          )}
+
+          {survey.wasteManagementProduction && (
+            <>
+              <h3>
+                Le producteur porte-t-il une attention particulière à la gestion de ses déchets lors
+                de la production ? Si oui, qu&apos;est ce qui est mis en place ?
+              </h3>
+              <Mdx code={survey.wasteManagementProduction} />
+            </>
+          )}
+
+          {survey.wasteManagementSales && (
+            <>
+              <h3>
+                Le producteur porte-t-il une attention particulière à la gestion de ses déchets lors
+                de la vente ? Si oui, qu&apos;est ce qui est mis en place ?
+              </h3>
+              <Mdx code={survey.wasteManagementSales} />
+            </>
+          )}
+
+          {survey.resourceConsumptionManagement && (
+            <>
+              <h3>
+                Le producteur fait-il attention à réguler sa consommation en eau et en énergie (gaz,
+                électricité, ...) ? Si oui, qu&apos;est ce qui est mis en place ?
+              </h3>
+              <Mdx code={survey.resourceConsumptionManagement} />
+            </>
+          )}
+
+          {survey.carbonImpactMeasurement && (
+            <>
+              <h3>
+                Le producteur est-il capable de mesurer l&apos;impact carbone de son exploitation,
+                de ses produits ? Si oui, quelle est-elle ?
+              </h3>
+              <Mdx code={survey.carbonImpactMeasurement} />
+            </>
+          )}
+
+          <Separator />
+        </div>
+      )}
+
+      {(survey.associativeEngagement || survey.jobSatisfaction || survey.additionalComment) && (
+        <div>
+          <h2>Engagement et démarche éthique</h2>
+
+          {survey.associativeEngagement && (
+            <>
+              <h3>Le producteur est-il engagé dans des démarches associatives ?</h3>
+              <Mdx code={survey.associativeEngagement} />
+            </>
+          )}
+
+          {survey.jobSatisfaction && (
+            <>
+              <h3>Le producteur et ses employés/associés sont-ils heureux dans leur travail ?</h3>
+              <Mdx code={survey.jobSatisfaction} />
+            </>
+          )}
+
+          {survey.additionalComment && (
+            <>
+              <h3>Un commentaire en plus ?</h3>
+              <Mdx code={survey.additionalComment} />
+            </>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+const StoreSurveyRender = (survey: StoreSurvey) => {
+  return (
+    <>
+      {survey.mainStoreValues && (
+        <>
+          <h3>
+            En trois mots : quelles sont les valeurs/idées principales portées par le producteur ?
+          </h3>
+          <p>{survey.mainStoreValues}</p>
+
+          <Separator />
+        </>
+      )}
+
+      {(survey.productOriginInterest ||
+        survey.productOriginLabeling ||
+        survey.wasteManagement ||
+        survey.transparencyOnProducerCompensation ||
+        survey.productLabelingChoice ||
+        survey.additionalComment) && (
+        <div>
+          <h2>Questions / Réponses</h2>
+
+          {survey.productOriginInterest && (
+            <>
+              <h3>Quel intérêt le commerçant accorde à la provenance des produits ?</h3>
+              <Mdx code={survey.productOriginInterest} />
+            </>
+          )}
+
+          {survey.productOriginLabeling && (
+            <>
+              <h3>La provenance des produits est-elle toujours indiquée ?</h3>
+              <Mdx code={survey.productOriginLabeling} />
+            </>
+          )}
+
+          {survey.wasteManagement && (
+            <>
+              <h3>
+                Comment se passe la gestion des déchets ? Quels dispositifs sont ou vont être mis en
+                place ?
+              </h3>
+              <Mdx code={survey.wasteManagement} />
+            </>
+          )}
+
+          {survey.transparencyOnProducerCompensation && (
+            <>
+              <h3>
+                Le commerçant est-il prêt à être transparent sur la rémunération des producteurs qui
+                fournissent le magasin ? Sur la manière dont sont fixés les prix des produits ?
+              </h3>
+              <Mdx code={survey.transparencyOnProducerCompensation} />
+            </>
+          )}
+
+          {survey.productLabelingChoice && (
+            <>
+              <h3>
+                Le magasin commercialise des produits avec des labels. Pourquoi ce choix et quels
+                sont ces labels ? Si ce n&apos;est pas le cas, pourquoi ?
+              </h3>
+              <Mdx code={survey.productLabelingChoice} />
+            </>
+          )}
+
+          {survey.additionalComment && (
+            <>
+              <h3>Un commentaire en plus ?</h3>
+              <Mdx code={survey.additionalComment} />
+            </>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+const TransformSurveyRender = (survey: TransformSurvey) => {
+  return (
+    <>
+      {survey.mainProducerValues && (
+        <>
+          <h3>
+            En trois mots : quelles sont les valeurs/idées principales portées par le producteur ?
+          </h3>
+          <p>{survey.mainProducerValues}</p>
+
+          <Separator />
+        </>
+      )}
+
+      {(survey.rawMaterialQualityRequirements ||
+        survey.rawMaterialOriginAndLabeling ||
+        survey.resourceConsumptionManagement ||
+        survey.wasteManagement) && (
+        <div>
+          <h2>Environnement</h2>
+
+          {survey.rawMaterialQualityRequirements && (
+            <>
+              <h3>
+                Quelles sont les exigences du producteur sur la qualité des matières premières
+                utilisées ?
+              </h3>
+              <Mdx code={survey.rawMaterialQualityRequirements} />
+            </>
+          )}
+
+          {survey.rawMaterialOriginAndLabeling && (
+            <>
+              <h3>
+                D&apos;où proviennent les matières premières ? La provenance et les fournisseurs
+                sont-ils indiqués sur les produits ?
+              </h3>
+              <Mdx code={survey.rawMaterialOriginAndLabeling} />
+            </>
+          )}
+
+          {survey.resourceConsumptionManagement && (
+            <>
+              <h3>
+                Le producteur fait-il attention à réguler sa consommation en eau et en énergie (gaz,
+                électricité, ...) ? Si oui, qu&apos;est ce qui est mis en place ?
+              </h3>
+              <Mdx code={survey.resourceConsumptionManagement} />
+            </>
+          )}
+
+          {survey.wasteManagement && (
+            <>
+              <h3>
+                Le producteur porte-t-il une attention particulière à la gestion de ses déchets lors
+                de la production/vente ? Si oui, qu&apos;est ce qui est mis en place ?
+              </h3>
+              <Mdx code={survey.wasteManagement} />
+            </>
+          )}
+
+          <Separator />
+        </div>
+      )}
+
+      {(survey.allergenLabeling ||
+        survey.producerLabels ||
+        survey.associativeEngagement ||
+        survey.jobSatisfaction ||
+        survey.additionalComment) && (
+        <div>
+          <h2>Engagement et démarche éthique</h2>
+
+          {survey.allergenLabeling && (
+            <>
+              <h3>Les allergènes des produits sont-ils systématiquement renseignés ?</h3>
+              <Mdx code={survey.allergenLabeling} />
+            </>
+          )}
+
+          {survey.producerLabels && (
+            <>
+              <h3>Le producteur possède-t-il des labels/appellations ? Si oui, le(s)quel(s) ?</h3>
+              <Mdx code={survey.producerLabels} />
+            </>
+          )}
+
+          {survey.associativeEngagement && (
+            <>
+              <h3>Le producteur est-il engagé dans des démarches associatives ?</h3>
+              <Mdx code={survey.associativeEngagement} />
+            </>
+          )}
+
+          {survey.jobSatisfaction && (
+            <>
+              <h3>Le producteur et ses employés/associés sont-ils heureux dans leur travail ?</h3>
+              <Mdx code={survey.jobSatisfaction} />
+            </>
+          )}
+
+          {survey.additionalComment && (
+            <>
+              <h3>Un commentaire en plus ?</h3>
+              <Mdx code={survey.additionalComment} />
+            </>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+const VegetalSurveyRender = (survey: VegetalSurvey) => {
+  return (
+    <>
+      {survey.mainProducerValues && (
+        <>
+          <h3>
+            En trois mots : quelles sont les valeurs/idées principales portées par le producteur ?
+          </h3>
+          <p>{survey.mainProducerValues}</p>
+
+          <Separator />
+        </>
+      )}
+
+      {(survey.cropOrganizationalOverview ||
+        survey.agroecologicalPractices ||
+        survey.greenhouseOrSoillessCultures ||
+        survey.phytosanitaryTreatments ||
+        survey.prophylacticMeasures) && (
+        <div>
+          <h2>Pratique culturale</h2>
+
+          {survey.cropOrganizationalOverview && (
+            <>
+              <h3>En quelques mots, comment s&apos;organise la culture ?</h3>
+              <Mdx code={survey.cropOrganizationalOverview} />
+            </>
+          )}
+
+          {survey.agroecologicalPractices && (
+            <>
+              <h3>
+                Des pratiques agroécologiques sont-elles mises en place ? Si oui, lesquelles ?
+              </h3>
+              <Mdx code={survey.agroecologicalPractices} />
+            </>
+          )}
+
+          {survey.greenhouseOrSoillessCultures && (
+            <>
+              <h3>
+                Des cultures hors sol ou dans des serres chauffées sont-elles présentes sur
+                l&apos;exploitation ? Si oui, comment s&apos;organisent-elles et dans quel but
+                sont-elles mises en place ?
+              </h3>
+              <Mdx code={survey.greenhouseOrSoillessCultures} />
+            </>
+          )}
+
+          {survey.phytosanitaryTreatments && (
+            <>
+              <h3>
+                Le producteur fait-il des traitements phytosanitaires ? Si oui, est-il d&apos;accord
+                pour les transmettre ?
+              </h3>
+              <Mdx code={survey.phytosanitaryTreatments} />
+            </>
+          )}
+
+          {survey.prophylacticMeasures && (
+            <>
+              <h3>
+                Le producteur met-il en place des mesures prophylactiques ? Si oui, lesquelles ?
+              </h3>
+              <Mdx code={survey.prophylacticMeasures} />
+            </>
+          )}
+
+          <Separator />
+        </div>
+      )}
+
+      {(survey.wasteManagementProduction ||
+        survey.wasteManagementSales ||
+        survey.energyConsumptionManagement ||
+        survey.waterConsumptionManagement ||
+        survey.carbonImpactMeasurement) && (
+        <div>
+          <h2>Environnement et ressources</h2>
+
+          {survey.wasteManagementProduction && (
+            <>
+              <h3>
+                Le producteur porte-t-il une attention particulière à la gestion de ses déchets lors
+                de la production ? Si oui, qu&apos;est ce qui est mis en place ?
+              </h3>
+              <Mdx code={survey.wasteManagementProduction} />
+            </>
+          )}
+
+          {survey.wasteManagementSales && (
+            <>
+              <h3>
+                Le producteur porte-t-il une attention particulière à la gestion de ses déchets lors
+                de la vente ? Si oui, qu&apos;est ce qui est mis en place ?
+              </h3>
+              <Mdx code={survey.wasteManagementSales} />
+            </>
+          )}
+
+          {survey.energyConsumptionManagement && (
+            <>
+              <h3>
+                Le producteur fait-il attention à réguler sa consommation en énergie (gaz,
+                électricité, ...) ? Si oui, qu&apos;est ce qui est mis en place ?
+              </h3>
+              <Mdx code={survey.energyConsumptionManagement} />
+            </>
+          )}
+
+          {survey.waterConsumptionManagement && (
+            <>
+              <h3>Comment est gérée la consommation d&apos;eau sur l&apos;exploitation ?</h3>
+              <Mdx code={survey.waterConsumptionManagement} />
+            </>
+          )}
+
+          {survey.carbonImpactMeasurement && (
+            <>
+              <h3>
+                Le producteur est-il capable de mesurer l&apos;impact carbone de son exploitation,
+                de ses produits ? Si oui, quelle est-elle ?
+              </h3>
+              <Mdx code={survey.carbonImpactMeasurement} />
+            </>
+          )}
+
+          <Separator />
+        </div>
+      )}
+
+      {(survey.associativeEngagement ||
+        survey.seasonalWorkers ||
+        survey.jobSatisfaction ||
+        survey.additionalComment) && (
+        <div>
+          <h2>Engagement et démarche éthique</h2>
+
+          {survey.associativeEngagement && (
+            <>
+              <h3>Le producteur est-il engagé dans des démarches associatives ?</h3>
+              <Mdx code={survey.associativeEngagement} />
+            </>
+          )}
+
+          {survey.seasonalWorkers && (
+            <>
+              <h3>Y a-t-il des travailleurs saisonniers sur l&apos;exploitation ?</h3>
+              <Mdx code={survey.seasonalWorkers} />
+            </>
+          )}
+
+          {survey.jobSatisfaction && (
+            <>
+              <h3>Le producteur et ses employés/associés sont-ils heureux dans leur travail ?</h3>
+              <Mdx code={survey.jobSatisfaction} />
+            </>
+          )}
+
+          {survey.additionalComment && (
+            <>
+              <h3>Un commentaire en plus ?</h3>
+              <Mdx code={survey.additionalComment} />
+            </>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
 const Producer = async ({ params }: { params: { slug: string } }) => {
   const producer = await getOneProducerBySlug({
     slug: params.slug,
@@ -64,6 +587,9 @@ const Producer = async ({ params }: { params: { slug: string } }) => {
   if (!producer) notFound();
 
   const data = producer.data.attributes;
+  const survey = data.survey[0];
+
+  // const likes = data.usersLikes.data.length;
 
   return (
     <>
@@ -178,11 +704,17 @@ const Producer = async ({ params }: { params: { slug: string } }) => {
 
         {/* TODO: Add producer information with hours, adress, etc. */}
         {/* TODO: Add product free */}
+        <Separator />
 
         {/* Article content */}
         <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
           <div className="max-w-3xl mx-auto">
-            <div className="mb-8">{/* <Mdx code={data.body.code} /> */}</div>
+            <div className="prose text-lg text-slate-500 max-w-none prose-lg prose-p:leading-normal prose-headings:font-playfair-display prose-headings:text-slate-900 prose-a:text-blue-600 prose-a:no-underline hover:prose-a:underline prose-a:font-medium prose-strong:font-medium prose-strong:text-slate-900 prose-blockquote:pl-4 prose-blockquote:border-l-2 prose-blockquote:border-slate-900 prose-blockquote:not-italic prose-blockquote:font-normal prose-blockquote:text-inherit before:prose-p:content-[''] after:prose-p:content-[''] prose-hr:my-8">
+              {survey?.__component === 'surveys.animal' && AnimalSurveyRender(survey)}
+              {survey?.__component === 'surveys.store' && StoreSurveyRender(survey)}
+              {survey?.__component === 'surveys.transform' && TransformSurveyRender(survey)}
+              {survey?.__component === 'surveys.vegetable' && VegetalSurveyRender(survey)}
+            </div>
           </div>
         </div>
       </article>
